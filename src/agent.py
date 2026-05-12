f7f6d381fb5c1a054078de4e341c3c770fc10f02
aneeshcheriank <aneeshcheriank@gmail.com>
2026-05-12 18:15:02 +0200

    jd extractor agent created

--- src/agent.py
+++ src/agent.py
@@ -1,5 +1,15 @@
 from src import prompts
 from src.model import get_llm
 from src.state import AgentState
+from src import prompts
+from src import inputs
 
-def jd_extractor():
+def jd_extractor(state: AgentState):
+    prompt = prompts.jd_keyword_extraction_prompt
+    llm = get_llm()
+    jd = inputs.get_jd()
+
+    chain = prompt | llm
+    response = chain.invoke({
+        "job_description": jd
+    })
