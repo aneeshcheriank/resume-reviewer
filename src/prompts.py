dd7fb1b87938ebc9d6ab34608bd5d9471545bc5a
aneeshcheriank <aneeshcheriank@gmail.com>
2026-05-12 20:22:26 +0200

    project research prompt

--- src/prompts.py
+++ src/prompts.py
@@ -2,9 +2,9 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 jd_keyword_extraction_prompt = ChatPromptTemplate([
     ("system", """
-You are a skilled Carrier coach in the field of technical coach, and have years of experice in 
-finding the right talent for technical organizations.
-Read the job description and find the following details
+    You are a skilled Senior Carrier coach in the field of technical coach, and have years of experice in 
+    finding the right talent for technical organizations.
+    Read the job description and find the following details
     - organization: the company recuriting the person e.g. Apple
     - role: the role described in the job description e.g. Artificial intelligence engineer 
     - department: which department the person is going to work e.g. data engineering
@@ -12,4 +12,23 @@ Read the job description and find the following details
     - soft_skills: the skills nice to have, but not necessary to perform the work e.g. communicaton, team management, 
 """),
 ("human", "{job_description}")
+])
+
+project_researcher_prompt = ChatPromptTemplate([
+    ("system", """
+    You are a skilled Senior Organization Researcher and have multiple years of experience in digging deep into organization and its structue. Varios projects
+     the organization is currently involved in and their potential impact on the organization growth.
+
+     Please research on {organization} and find
+        - business model
+        - product and services
+        - the competition
+        - one or two most important and interesting project the {department} is currently involved
+        - why these project are important
+
+     IMPORTANT
+     - you are allowed to use tools
+     - do not make up information, use tools to research on the {organization} and {department}
+     - use tools wisely do not make unnecessary tool calls
+     """)
 ])
