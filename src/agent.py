7a2b3a01e5e87c43dabc072ad89850914823e2c6
aneeshcheriank <aneeshcheriank@gmail.com>
2026-05-12 19:29:45 +0200

    updated the jd extractor with structured output

--- src/agent.py
+++ src/agent.py
@@ -3,13 +3,15 @@ from src.model import get_llm
 from src.state import AgentState
 from src import prompts
 from src import inputs
+from src import schema
 
 def jd_extractor(state: AgentState):
     prompt = prompts.jd_keyword_extraction_prompt
     llm = get_llm()
+    llm_with_structured_output = llm.with_structured_output(schema=schema.JDExtractorSchema)
     jd = inputs.get_jd()
 
-    chain = prompt | llm
+    chain = prompt | llm_with_structured_output
     response = chain.invoke({
         "job_description": jd
     })
