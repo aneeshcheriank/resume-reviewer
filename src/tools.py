42226d517f55e6056aefccf9e08f96ffabdc2453
aneeshcheriank <aneeshcheriank@gmail.com>
2026-05-13 18:58:25 +0200

    search tool

--- src/tools.py
+++ src/tools.py
@@ -1,6 +1,6 @@
-from langchain_community.tools import DuckDuckGoSearchResult
+from langchain_community.tools import DuckDuckGoSearchResults
 
-search_tool = DuckDuckGoSearchResult(max_results=3)
+search_tool = DuckDuckGoSearchResults(max_results=3)
 
 project_research_tool_list = [search_tool]
 project_research_tool_mapping = {tool.name: tool for tool in project_research_tool_list}
