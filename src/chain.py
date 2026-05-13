6e78dbebc71185bec4c376687ef69f05b4f03699
aneeshcheriank <aneeshcheriank@gmail.com>
2026-05-13 18:56:15 +0200

    jd_extractor agent + project research agent

--- src/chain.py
+++ src/chain.py
@@ -1,14 +1,29 @@
 from langgraph.graph import StateGraph, START, END
 from src.state import AgentState
+from src import agent
 
 def built_graph():
     workflow = StateGraph(AgentState)
 
     #nodes
+    workflow.add_node("jd_extractor", agent.jd_extractor)
+    workflow.add_node("project_researcher", agent.project_researcher)
+    workflow.add_node("tool_call_project_research", agent.tool_call_project_research)
+    workflow.add_node("project_summarizer", agent.project_summarizer)
+
 
     #edges
+    workflow.add_edge(START, "jd_extractor")
+    workflow.add_edge("jd_extractor", "project_researcher")
+    workflow.add_edge("tool_call_project_research", "project_researcher")
+    workflow.add_edge("project_summarizer", END)
 
     #conditional edges
+    workflow.add_conditional_edges("project_researcher", agent.router_project_research,
+                                   {
+                                       "tool_call_project_research": "tool_call_project_research",
+                                       "project_summarizer": "project_summarizer"
+                                   })
 
     compiled_workflow = workflow.compile()
 
