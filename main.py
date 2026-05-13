32238fda141da393cc6d4b67276b6e55855a1424
aneeshcheriank <aneeshcheriank@gmail.com>
2026-05-13 18:55:01 +0200

    updated main to incorporate chain output

--- main.py
+++ main.py
@@ -1,6 +1,51 @@
-from src import agent
-from src.state import AgentState
+from src import chain
+from src.inputs import get_jd
 
 if __name__== "__main__":
-    response = agent.jd_extractor(AgentState)
-    print(response)
+    jd = get_jd()
+    build_cahin = chain.built_graph()
+    response = build_cahin.invoke({
+        "query": "start the process",
+
+        "resume": "",
+        "job_description": jd,
+        "cover_letter": "",
+    
+        # jd extractor
+        "organization": "",
+        "role": "",
+        "department": "",
+        "hard_skills": "",
+        "soft_skills": "",
+        "keywords": "",
+    
+        # project_research
+        "research_history": [],
+        "projects": [],
+        "business_model": "",
+        "product": "",
+        "product_and_services": "",
+        "competition": "",
+        "project_research_iteration": 0
+    })
+
+    keys  = [
+        "organization",
+        "role",
+        "department",
+        "hard_skills",
+        "soft_skills",
+        "keywords",
+    
+        # project_research
+        "projects",
+        "business_model",
+        "product",
+        "product_and_services",
+        "competition"
+    ]
+    for key in keys:
+        print(response.get(key))
+        print("\n")
+
+    
