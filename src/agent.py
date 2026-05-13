463ed1ee34f08d5ca94ca022ea194ea8b9995074
aneeshcheriank <aneeshcheriank@gmail.com>
2026-05-13 20:07:44 +0200

    added formatter agent for researcher

--- src/agent.py
+++ src/agent.py
@@ -4,7 +4,7 @@ from src.state import AgentState
 from src import inputs
 from src import schema
 from src.utility import tool_call_node, router
-from src import tools
+from src import tools, config
 
 def jd_extractor(state: AgentState):
     prompt = prompts.jd_keyword_extraction_prompt
@@ -21,19 +21,25 @@ def jd_extractor(state: AgentState):
 
     return {
         "jd": jd,
-        "org": output.get("org", ""),
+        "organization": output.get("org", ""),
         "role": output.get("role", ""),
         "department": output.get("department", ""),
-        "hard_skills": output.get("hard_skills", ""),
-        "soft_skills": output.get("soft_skills", ""),
-        "key_words": output.get("key_words", "")
+        "hard_skills": output.get("hard_skills", []),
+        "soft_skills": output.get("soft_skills", []),
+        "keywords": output.get("keywords", [])
     }
 
 def project_researcher(state: AgentState):
     prompt = prompts.project_researcher_prompt
     llm = get_llm()
+    llm_with_tools = llm.bind_tools([tools.search_tool])
+    iteration = state["project_research_iteration"]
+    
+    if iteration < config.max_search_calls:
+        chain = prompt | llm_with_tools
+    else:
+        chain = prompt | llm
 
-    chain = prompt | llm
     response = chain.invoke({
         "organization": state["organization"],
         "department": state["department"]
@@ -59,4 +65,24 @@ def project_summarizer(state: AgentState):
 
     return{
         "research_history": [response]
+    }
+
+def project_formatter(state: AgentState):
+    prompt = prompts.project_formatter_prompt
+    llm = get_llm()
+    llm_with_structured_output = llm.with_structured_output(schema.ProjectResearcher)
+    last_history = state["research_history"][-1]
+
+    chain = prompt | llm_with_structured_output
+    response = chain.invoke({
+        "summary": last_history
+    })
+
+    output = response.model_dump()
+
+    return{
+         "projects": output.get("projects"),
+         "business_model": output.get("business_model"),
+         "product_and_services": output.get("product_and_services"),
+         "competition": output.get("competition")
     }
