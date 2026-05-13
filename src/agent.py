9e1c29e838bae1047521ae68fe41428d6470b7cc
aneeshcheriank <aneeshcheriank@gmail.com>
2026-05-13 18:54:28 +0200

    added a summurizing agent to summarize the tool calls

--- src/agent.py
+++ src/agent.py
@@ -3,7 +3,8 @@ from src.model import get_llm
 from src.state import AgentState
 from src import inputs
 from src import schema
-from src.utility import tool_call_node
+from src.utility import tool_call_node, router
+from src import tools
 
 def jd_extractor(state: AgentState):
     prompt = prompts.jd_keyword_extraction_prompt
@@ -12,7 +13,10 @@ def jd_extractor(state: AgentState):
     jd = inputs.get_jd()
 
     chain = prompt | llm_with_structured_output
-    response = chain.invoke(state)
+    response = chain.invoke({
+        "job_description": state['job_description'],
+        "query": state["query"]
+    })
     output = response.model_dump()
 
     return {
@@ -33,6 +37,24 @@ def project_researcher(state: AgentState):
     response = chain.invoke({
         "organization": state["organization"],
         "department": state["department"]
+    })
+
+    return{
+        "research_history": [response]
+    }
+
+tool_call_project_research = tool_call_node("research_history", "project_research_iteration", tools.project_research_tool_mapping)
+router_project_research = router("research_history", map={
+    "tool_call": "tool_call_project_research", "default": "project_summarizer"
+})
+
+def project_summarizer(state: AgentState):
+    prompt = prompts.summary_research_prompt
+    llm = get_llm()
+
+    chain = prompt | llm
+    response = chain.invoke({
+        "chat_history": state["research_history"]
     })
 
     return{
