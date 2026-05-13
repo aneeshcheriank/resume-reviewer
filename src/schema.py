e50e71abc32abc642ded39ec7c8f24d90dcdb835
aneeshcheriank <aneeshcheriank@gmail.com>
2026-05-13 18:57:17 +0200

    project researcher format

--- src/schema.py
+++ src/schema.py
@@ -7,3 +7,9 @@ class JDExtractorSchema(BaseModel):
     hard_skills: str = Field(description="the organization in which the postion is vaccant")
     soft_skills: str = Field(description="the organization in which the postion is vaccant")
     keywords: str = Field(description="the organization in which the postion is vaccant")
+
+class ProjectResearcher(BaseModel):
+    business_model: str = Field(description="Business of the organization")
+    product_and_services: str = Field(description="Product and services of the organization")
+    competition: str = Field(description="Compatition and competetors for the organization")
+    important_projects: list[str] = Field(description="list of important projects the department is doing for the organization and why the project is important")
