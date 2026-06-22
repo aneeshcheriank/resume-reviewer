6f7bf5fa540f8f053645d016f128294f4159dff4
aneeshcheriank <aneeshcheriank@gmail.com>
2026-06-22 14:32:19 +0200

    model name fixed; deepseek-chat

--- src/model.py
+++ src/model.py
@@ -7,7 +7,7 @@ def get_llm():
     config_env()
 
     return ChatDeepSeek(
-        model="deepseek-v4-flash",
+        model="deepseek-chat",
         temperature=0.0,
         api_key=os.environ.get("DEEPSEEK"),
         extra_body={"thinking": { # to disable the reasoning (creates problems in formating tool)
