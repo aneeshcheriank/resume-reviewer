17fe5e8150ae93930522874ba46308f439e71adb
aneeshcheriank <aneeshcheriank@gmail.com>
2026-06-22 14:31:45 +0200

    removed the error message. LLM may tries to extract details from error message, confusing that the error message is the content of the file

--- src/inputs.py
+++ src/inputs.py
@@ -4,12 +4,8 @@ import re
 from src import config
 
 def get_jd(jd_path=config.jd_path):
-    try:
-        with open(jd_path, "r") as f:
-            file = f.read()
-        return file
-    except Exception as e:
-        return f"Exception: {e}"
+    with open(jd_path, "r") as f:
+        return f.read()
 
 def get_resume(resume_path):
     doc = fitz.open(resume_path)
