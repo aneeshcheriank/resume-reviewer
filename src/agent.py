a0ad593c1156e9afc0e92bb1d33ce777639b9c76
aneeshcheriank <aneeshcheriank@gmail.com>
2026-06-22 14:16:41 +0200

    input jd is removed from agent node

--- src/agent.py
+++ src/agent.py
@@ -3,7 +3,6 @@ from langgraph.graph import END
 from src import prompts
 from src.model import get_llm
 from src.state import AgentState
-from src import inputs
 from src import schema
 from src.utility import tool_call_node, router
 from src import tools, config
@@ -12,7 +11,6 @@ def jd_extractor(state: AgentState):
     prompt = prompts.jd_keyword_extraction_prompt
     llm = get_llm()
     llm_with_structured_output = llm.with_structured_output(schema=schema.JDExtractorSchema)
-    jd = inputs.get_jd()
 
     chain = prompt | llm_with_structured_output
     response = chain.invoke({
@@ -21,7 +19,6 @@ def jd_extractor(state: AgentState):
     output = response.model_dump()
 
     return {
-        "jd": jd,
         "organization": output.get("organization", ""),
         "role": output.get("role", ""),
         "department": output.get("department", ""),
